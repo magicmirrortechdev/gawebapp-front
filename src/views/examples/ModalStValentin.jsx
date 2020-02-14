74f3969031ec5663db72dcb873277e05b6e870b1
José Luis Palillero Huerta <joseluispalillero@gmail.com>
2020-02-14 11:00:52 -0600

    modificacion de modales

--- src/views/examples/ModalStValentin.jsx
+++ src/views/examples/ModalStValentin.jsx
@@ -1,32 +1,33 @@
-import React, { useState } from 'react';
-import {  Modal, ModalHeader, ModalBody } from 'reactstrap';
+import React, {useState} from 'react';
+import {Modal, ModalHeader, ModalBody} from 'reactstrap';
 import image from './sanv.jpg'
 
 const ModalStValentin = (props) => {
-  const {
-    className,
-    lg
-  } = props;
+    const {
+        className,
+        lg
+    } = props;
 
-  const [modalStValentin, setModal] = useState(true);
+    const [modalStValentin, setModalStValentin] = useState(true);
 
-  const toggle = () => setModal(!modalStValentin);
+    const toggleSnValentin = () => setModalStValentin(!modalStValentin);
 
-  const closeBtn = <button className="close" onClick={toggle}>&times;</button>;
+    const closeModalSnValentinBtn = <button className="close" onClick={toggleSnValentin}>&times;</button>;
 
-  return (
- 
-    <Modal style={{paddingTop: '110px', display: "flex", alignContent:"center", alignItems:"center"}} 
-      isOpen={modalStValentin} toggle={toggle} className={className} size={lg}>
-      <ModalHeader style={{paddingLeft:"100px", paddingTop:"40px", paddingBottom:"10px"}} toggle={toggle} close={closeBtn}>
-      <p style={{fontSize:"20px"}}>On behalf of Magic Mirror's team</p>
-      </ModalHeader>
-      <ModalBody>
-        <img height="100%" width="100%" src={image} alt="San Valentin"/>
-      </ModalBody>
-    </Modal>
+    return (
 
-);
+        <Modal style={{paddingTop: '110px', display: "flex", alignContent: "center", alignItems: "center"}}
+               isOpen={modalStValentin} toggle={toggleSnValentin} className={className} size={lg}>
+            <ModalHeader style={{paddingLeft: "100px", paddingTop: "40px", paddingBottom: "10px"}}
+                         toggle={toggleSnValentin} close={closeModalSnValentinBtn}>
+                <p style={{fontSize: "20px"}}>On behalf of Magic Mirror's team</p>
+            </ModalHeader>
+            <ModalBody>
+                <img height="100%" width="100%" src={image} alt="San Valentin"/>
+            </ModalBody>
+        </Modal>
+
+    );
 }
 
 export default ModalStValentin;
