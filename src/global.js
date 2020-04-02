b2ed311747d1b1640822a782e05e409765cd933f
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2020-04-02 12:18:50 -0600

    ok url

--- src/global.js
+++ src/global.js
@@ -3,12 +3,12 @@ let Global = {
     //url: "https://green-acorn-production.herokuapp.com/"
     //url: "https://greenacorn.herokuapp.com/"
 
-    urlArgyle: "http://localhost:9000", //local
-    //urlArgyle: "https://argyle-api-dev.herokuapp.coms" //staging
+    //urlArgyle: "http://localhost:9000", //local
+    urlArgyle: "https://argyle-api-dev.herokuapp.coms", //staging
     //urlArgyle: "" //production
     merchantId: "5ddbe4b14d581e0017f78c46" //local
-    //merchantId: "5ddbe4b14d581e0017f78c46" // staging
-    //merchantId: "" //production
+        //merchantId: "5ddbe4b14d581e0017f78c46" // staging
+        //merchantId: "" //production
 };
 
 export default Global;
