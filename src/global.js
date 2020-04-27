915722b361d1142b6f75f7ae86160960c1ecf8f8
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2020-04-27 18:10:48 -0500

    ok mails gaby

--- src/global.js
+++ src/global.js
@@ -2,6 +2,7 @@ let Global = {
     //url: "http://localhost:3000/",
     //url: "https://green-acorn-production.herokuapp.com/",
     url: "https://greenacorn.herokuapp.com/",
+    //url: "https://green-acorn-app.herokuapp.com/",
 
     //urlArgyle: "http://localhost:9000", //local
     urlArgyle: "https://argyle-api-dev.herokuapp.com", //staging
