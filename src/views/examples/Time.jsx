881fc3218290b0f95c7f98043b5089828dbc4efc
José Luis Palillero Huerta <joseluispalillero@gmail.com>
2020-06-26 13:52:03 -0500

    change times component

--- src/views/examples/Time.jsx
+++ src/views/examples/Time.jsx
@@ -17,11 +17,36 @@ import {
 // core components
 import Header from "components/Headers/Header.jsx";
 import Global from "../../global";
+import Moment from "react-moment";
 let loggedUser
 loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
-
+let times = [];
 
 class Time extends React.Component {
+  compareValues(key, order = 'asc') {
+    return function innerSort(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        // property doesn't exist on either object
+        return 0;
+      }
+
+      const varA = (typeof a[key] === 'string')
+          ? a[key].toUpperCase() : a[key];
+      const varB = (typeof b[key] === 'string')
+          ? b[key].toUpperCase() : b[key];
+
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return (
+          (order === 'desc') ? (comparison * -1) : comparison
+      );
+    };
+  }
+
   state = {
     jobs:[]
   };
@@ -69,6 +94,34 @@ class Time extends React.Component {
   render() {
 
     if (!this.state) return <p>Loading</p>
+    else {
+      this.state.jobs.map((e,i) => {
+        console.log(e);
+        let projectManager = e.projectManager.map((e,i)=> !projectManager ? <p style={{fontSize:"10px"}}>Project Manager Delete</p> : <p style={{fontSize:"10px"}} key={i}>{e.projectId.name}</p>)
+        if(e.workers.length > 0){
+          e.workers.map((worker, i ) => {
+            console.log("worker ", worker);
+            worker.time.map((time, i) => {
+              times.push({
+                date: time.date,
+                time: time.hours,
+                jobName: e.jobName,
+                worker: worker._id,
+                name: worker.workerId.name,
+                projectManager: projectManager,
+                estimateId: e._id,
+                workerId: worker.workerId,
+                workers: e.workers
+              });
+            });
+          })
+        }
+      });
+
+      times = times.sort(this.compareValues('date','desc'));
+
+      console.log(times);
+    }
     return (
       <>
         <Header />
@@ -103,91 +156,81 @@ class Time extends React.Component {
                   <thead className="thead-light">
                     <tr>
                       <th scope="col"></th>
-
+                      <th scope="col">Date</th>
                       <th scope="col">Name</th>
-                      <th scope="col">Total Hours</th>
+                      <th scope="col">Hours</th>
                       <th scope="col">Job</th>
                     </tr>
                   </thead>
                   
-                    
-
-                     {this.state.jobs.length === 0 ?  <tbody><tr><td>No workers register</td></tr></tbody>:
-                     this.state.jobs.map((e,i)=>{
-                      if(!e.workers)return <th scope="row">Worker Delete</th>
+                     {times.length === 0 ?  <tbody><tr><td>No workers register</td></tr></tbody>:
+                     times.map((e,i)=>{
                        let jobName = <p style={{fontSize:"10px"}} key={i}>{e.jobName}</p>
-                       let projectManager = e.projectManager.map((e,i)=>!projectManager ? <p style={{fontSize:"10px"}}>Project Manager Delete</p> : <p style={{fontSize:"10px"}} key={i}>{e.projectId.name}</p>)
-                       let estimateId = e._id    
+                       let projectManager = e.projectManager
+                       let estimateId = e.estimateId
                        let newWorker =e.workers.filter(wx => wx.workerId && wx.workerId._id === loggedUser._id)
-                   
+
                       return(
-
-                        
-                        loggedUser.level >= 2 ? e.workers.map((e,i)=>{
-                        let time = <p style={{fontSize:"10px"}}>{e.time.reduce((acc, current, i) => acc + current.hours, 0) ? e.time.reduce((acc, current, i) => acc + current.hours, 0) : 0 }</p>
-
-                          if(!e.workerId)return <th scope="row">Worker Delete</th>
-                          let worker = e.workerId._id
-                          return(
+                        loggedUser.level >= 2 ?
                         <tbody key={i}>
-                        <tr>
-                        <td >
-                          <UncontrolledDropdown>
-                           <DropdownToggle>
-                              ...
-                          </DropdownToggle>
-                          <DropdownMenu
-                            modifiers={{
-                                    setMaxHeight: {
-                                      enabled: true,
-                                      order: 890,
-                                      fn: (data) => {
-                                        return {
-                                          ...data,
-                                          styles: {
-                                            ...data.styles,
-                                            overflow: 'auto',
-                                            maxHeight: 100,
+                          <tr>
+                            <td >
+                              <UncontrolledDropdown>
+                               <DropdownToggle>
+                                  ...
+                              </DropdownToggle>
+                              <DropdownMenu
+                                modifiers={{
+                                        setMaxHeight: {
+                                          enabled: true,
+                                          order: 890,
+                                          fn: (data) => {
+                                            return {
+                                              ...data,
+                                              styles: {
+                                                ...data.styles,
+                                                overflow: 'auto',
+                                                maxHeight: 100,
+                                              },
+                                            };
                                           },
-                                        };
-                                      },
-                                    },
-                                  }}
-                                                        >
-                          
-                          <DropdownItem to={`/admin/time/addtime/${estimateId}/${e._id}/${e.workerId._id}`} tag={Link}>Add Hours</DropdownItem>
+                                        },
+                                      }}
+                                                            >
 
-                            { loggedUser.level >= 4 ?
-                              <DropdownItem onClick={()=>{
-                              axios.patch(Global.url + `workerdelete/${estimateId}/${e._id}`, {worker: worker})
-                                .then(({data}) => {
-                                  
-                                  window.location.reload()
-                                  alert('Worker Removed')                               
-                                })
-                                .catch(err => {
-                                  console.log(err.response)
-                                })
-                            }}><span
-                                    className="text-danger">Delete</span>
-                              </DropdownItem>
-                              : null
-                            }
+                              <DropdownItem to={`/admin/time/addtime/${estimateId}/${e.worker}/${e.workerId._id}`} tag={Link}>Add Hours</DropdownItem>
 
-                          </DropdownMenu>
-                          </UncontrolledDropdown>
-                        </td>
-                        <th scope="row">{e.workerId.name}</th>
-                        <td style={{height:"100%",paddingTop:"35px", paddingLeft:"60px", display:"flex", flexDirection:"column", alignItems:"baseline", alignContent:"center"}}>
-                        {time}</td>
-                        <td style={{height:"100%",paddingTop:"35px", paddingLeft:"60px"}} >
-                             {jobName}
-                        </td>                        
-                       
-                        </tr>
-                      </tbody>
-                          )
-                        }) :
+                                { loggedUser.level >= 4 ?
+                                  <DropdownItem onClick={()=>{
+                                  axios.patch(Global.url + `workerdelete/${estimateId}/${e.worker}`, {worker: e.workerId._id})
+                                    .then(({data}) => {
+
+                                      window.location.reload()
+                                      alert('Worker Removed')
+                                    })
+                                    .catch(err => {
+                                      console.log(err.response)
+                                    })
+                                }}><span
+                                        className="text-danger">Delete</span>
+                                  </DropdownItem>
+                                  : null
+                                }
+
+                              </DropdownMenu>
+                              </UncontrolledDropdown>
+                            </td>
+                            <td scope="row"><Moment add={{days:1}} format={"MMM D, YY"}>{e.date}</Moment></td>
+                            <td scope="row">{e.name}</td>
+                            <td style={{height:"100%",paddingTop:"35px", paddingLeft:"60px", display:"flex", flexDirection:"column", alignItems:"baseline", alignContent:"center"}}>
+                            {e.time}</td>
+                            <td style={{height:"100%",paddingTop:"35px", paddingLeft:"60px"}} >
+                                 {e.jobName}
+                            </td>
+
+                          </tr>
+                        </tbody>
+                         :
                         loggedUser.level <= 1 ?
                         newWorker.map((e,i)=>{
                         let time = <p style={{fontSize:"10px"}}>{e.time.reduce((acc, current, i) => acc + current.hours, 0) ? e.time.reduce((acc, current, i) => acc + current.hours, 0) : 0 }</p>
