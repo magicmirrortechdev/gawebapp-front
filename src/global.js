33e99b8ff30aa4186bd8479c0589d7d261ad6d39
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2020-07-17 11:49:52 -0500

    push staging

--- src/global.js
+++ src/global.js
@@ -1,20 +1,20 @@
 let Global = {
-    url: "http://localhost:3000/",
-    //url: "https://greenacorn.herokuapp.com/",
+    //url: "http://localhost:3000/",
+    url: "https://greenacorn.herokuapp.com/",
     //url: "https://green-acorn-app.herokuapp.com/",
 
 
     //testing && production
-    urlEnvironment: "local",
-    //urlEnvironment: "staging",
+    //urlEnvironment: "local",
+    urlEnvironment: "staging",
     //urlEnvironment: "production",
 
-    urlArgyle: "https://argyle-api-dev.herokuapp.com", //local
-    //urlArgyle: "https://argyle-api-dev.herokuapp.com", //staging
+    //urlArgyle: "https://argyle-api-dev.herokuapp.com", //local
+    urlArgyle: "https://argyle-api-dev.herokuapp.com", //staging
     //urlArgyle: "https://api.roverpayapp.com", //production
 
-    merchantId: "5ddbe4b14d581e0017f78c46", //local
-    //merchantId: "5ddbe4b14d581e0017f78c46", // staging
+    //merchantId: "5ddbe4b14d581e0017f78c46", //local
+    merchantId: "5ddbe4b14d581e0017f78c46", // staging
     //merchantId: "5df7a89cbd23660017c29f1f" //production 
 };
 
