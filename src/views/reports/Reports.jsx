b0e57080b6c27b56157b38b3767b5188d72b1532
José Luis Palillero Huerta <joseluispalillero@gmail.com>
2020-09-14 13:33:45 -0500

    update version v 2.6.3

--- src/views/reports/Reports.jsx
+++ src/views/reports/Reports.jsx
@@ -210,48 +210,47 @@ class Reports extends React.Component {
         this.props.getUsers();
         document.getElementById('spinner').style.visibility='visible';
 
-        setTimeout(function(){
-            let jobsFilter = this.props.jobs.filter(job => job.status === 'Approve');
-            jobsFilter.forEach(job =>{
-                if(job.expenses.length > 0){
-                    job.expenses = job.expenses.filter(expenses =>
-                        expenses.date >= this.state.startDate && expenses.date <= this.state.endDate
+        let jobsFilter = this.props.jobs.filter(job => job.status === 'Approve');
+        jobsFilter.forEach(job =>{
+            if(job.expenses.length > 0){
+                job.expenses = job.expenses.filter(expenses =>
+                    expenses.date >= this.state.startDate && expenses.date <= this.state.endDate
+                )
+            }
+
+            if(job.invoices.length > 0){
+                job.invoices = job.invoices.filter(invoices =>
+                    invoices.date >= this.state.startDate && invoices.date <= this.state.endDate
+                )
+            }
+
+            if(job.workers.length > 0){
+                job.workers.forEach(worker =>{
+                    worker.time = worker.time.filter(time =>
+                        time.date >= this.state.startDate && time.date <= this.state.endDate
                     )
-                }
+                })
+                job.workers = job.workers.filter(worker => worker.time.length > 0)
+            }
+        })
 
-                if(job.invoices.length > 0){
-                    job.invoices = job.invoices.filter(invoices =>
-                        invoices.date >= this.state.startDate && invoices.date <= this.state.endDate
-                    )
-                }
+        jobsFilter = jobsFilter.sort(compareValues('jobName', 'asc'));
 
-                if(job.workers.length > 0){
-                    job.workers.forEach(worker =>{
-                        worker.time = worker.time.filter(time =>
-                            time.date >= this.state.startDate && time.date <= this.state.endDate
-                        )
-                    })
-                    job.workers = job.workers.filter(worker => worker.time.length > 0)
-                }
-            })
-
-            jobsFilter = jobsFilter.sort(compareValues('jobName', 'asc'));
-
-            let workers = this.props.users.filter(user => user.role === "WORKER" ||
-                user.role ==="PROJECT MANAGER" ||
-                user.role ==="ADMIN");
+        let workers = this.props.users.filter(user => user.role === "WORKER" ||
+            user.role ==="PROJECT MANAGER" ||
+            user.role ==="ADMIN");
 
 
-            this.setState(prevState => {
-                return {
-                    ...prevState,
-                    loadFilter: true,
-                    jobs: jobsFilter,
-                    workers: this.workersTransformer(workers, this.state.startDate, this.state.endDate)
-                }
-            })
-            document.getElementById('spinner').style.visibility='hidden';
-        }, 2000)
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                loadFilter: true,
+                jobs: jobsFilter,
+                workers: this.workersTransformer(workers, this.state.startDate, this.state.endDate)
+            }
+        })
+        document.getElementById('spinner').style.visibility='hidden';
+
     }
 
     clearFilter = () => {
